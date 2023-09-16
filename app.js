16d7ba3e487cec33c91134883eaafd33a1e0e0f0
Bagus Cahyono <baguscyy@gmail.com>
2023-09-16 07:17:11 +0000

    add:function

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ function sum(a, b) {
     return a + b;
 }
 sum(1, 2); // 3
+
+//function isprime
+function isPrime(num) {
+    for (let i = 2; i < num; i++)
+        if (num % i === 0) return false;
+    return num > 1;
+}
